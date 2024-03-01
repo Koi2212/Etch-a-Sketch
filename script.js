1c8517ae0f5ad9700867ad132a346978aa6ab084
Koi2212 <funnymine2212@gmail.com>
2024-03-01 20:33:25 +0700

    Done!

--- script.js
+++ script.js
@@ -9,8 +9,25 @@ function randomColor() {
 }
 
 //Lệnh lấy số ô người dùng nhập vào
+let numberOfBox = 1;
+const playingSpace = document.querySelector(".box-container");
+
 document
   .querySelector("div button:first-child")
   .addEventListener("click", function () {
-    console.log(document.querySelector("input").value);
+    let listClean = document.querySelectorAll(".box-container div");
+    let arr = Array.from(listClean);
+    for (let i = 0; i < arr.length; i++) {
+      playingSpace.removeChild(arr[i]);
+    }
+    numberOfBox = document.querySelector("input").value;
+    for (let i = 0; i < numberOfBox * numberOfBox; i++) {
+      let newElement = document.createElement("div");
+      newElement.style.width = (1 / numberOfBox) * 100 + "%";
+      newElement.style.height = (1 / numberOfBox) * 100 + "%";
+      newElement.addEventListener("mouseover", function () {
+        newElement.style.backgroundColor = randomColor();
+      });
+      playingSpace.appendChild(newElement);
+    }
   });
